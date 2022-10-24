6719226735027915883bfbe78fe2e344aeba5faf
lil-dev16 <olusegunkd2017@gmail.com>
2022-10-24 15:17:19 +0100

    added icons

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import Image from '../segun.jfif'
+import Image from '../profile__img.jpg'
 
 function Profile() {
   return (
     <div className="profile">
       <img src={Image} alt="profile img" id="profile__img"/>
       <p id="twitter" style={{fontWeight: 700,fontSize: '20px',lineHeeight: '30px'}}>segun0x</p>
-      <p id="slack">segunn</p>
+      <p id="slack">olusegunkd2017</p>
     </div>
   )
 }
--- src/components/Socials.jsx
+++ src/components/Socials.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { FaSlack } from "react-icons/fa";
+import { FaGithub } from 'react-icons/fa';
 
 export const Socials = () => {
   return (
-    <div>
-        
+    <div className='social-icons'>
+        <FaGithub />
+        <FaSlack />
     </div>
   )
 }
