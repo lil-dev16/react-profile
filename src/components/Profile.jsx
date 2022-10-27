6d005240b57d69a7a3493cc9d1619c27bfae1a3d
lil-dev16 <olusegunkd2017@gmail.com>
2022-10-27 11:37:36 +0100

    fixes

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -6,7 +6,7 @@ function Profile() {
     <div className="profile">
       <img src={Image} alt="profile img" id="profile__img"/>
       <p id="twitter" style={{fontWeight: 700,fontSize: '20px',lineHeeight: '30px'}}>segun0x</p>
-      <p id="slack">olusegunkd2017</p>
+      <p id="slack">segun0x</p>
     </div>
   )
 }
